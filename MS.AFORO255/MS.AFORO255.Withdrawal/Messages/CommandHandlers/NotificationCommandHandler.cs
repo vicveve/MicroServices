9aa67cbfeae0711338f65b1ccb61b1079428899a
vicveve <vvictor_ernesto@hotmail.com>
2023-06-19 16:44:12 -0600

    Gateway
    Gateway

---
+++ MS.AFORO255/MS.AFORO255.Withdrawal/Messages/CommandHandlers/NotificationCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Aforo255.Cross.Event.Src.Bus;
+using MediatR;
+using MS.AFORO255.Withdrawal.Messages.Commands;
+using MS.AFORO255.Withdrawal.Messages.Events;
+
+namespace MS.AFORO255.Withdrawal.Messages.CommandHandlers
+{
+    public class NotificationCommandHandler : IRequestHandler<NotificationCreateCommand, bool>
+    {
+        private readonly IEventBus _bus;
+
+        public NotificationCommandHandler(IEventBus bus) => _bus = bus;
+
+        public Task<bool> Handle(NotificationCreateCommand request, CancellationToken cancellationToken)
+        {
+            _bus.Publish(new NotificationCreatedEvent(request.IdTransaction, request.Amount, request.Type,
+                    "", "", request.AccountId));
+            return Task.FromResult(true);
+        }
+    }
+}
